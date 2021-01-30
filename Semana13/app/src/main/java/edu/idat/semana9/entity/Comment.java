420c6b22edf317d1ef878f04177914f0f2ac5a73
Andy <adiazval20@gmail.com>
2021-01-29 20:03:30 -0500

    5

--- Semana13/app/src/main/java/edu/idat/semana9/entity/Comment.java
+++ Semana13/app/src/main/java/edu/idat/semana9/entity/Comment.java
@@ -1,8 +1,14 @@
 package edu.idat.semana9.entity;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Comment {
     private String id;
     private String message;
+
+    @SerializedName("owner")
+    private User user;
+
     private long postId;
 
     public String getId() {
